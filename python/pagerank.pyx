0d37d3a1bf279ecf8cf90a6fdb4ee9f88da9f853
Shane Evans <shane.evans@gmail.com>
2015-03-24 12:50:36 +0900

    tweak cython to produce better code
    In the loop where most of the time was spent (updating pagerank calculations), the previous version was accessing data via numpy methods and creating objects. This tweak brings it a bit closer to what the C code does.

--- python/pagerank.pyx
+++ python/pagerank.pyx
@@ -2,6 +2,7 @@ import tempfile
 import bcolz
 import numpy as np
 cimport numpy as np
+cimport cython
 
 cdef packed struct Edge:
     np.int64_t start
@@ -13,22 +14,29 @@ class PageRank(object):
         self.damping = damping
 
         N = int(n_pages)
+
         self.pr1 = np.memmap('/dev/zram0', dtype=np.float32, mode='w+', shape=(N,))
         self.pr2 = np.memmap('/dev/zram1', dtype=np.float32, mode='w+', shape=(N,))
         self.out_degree = np.memmap('/dev/zram2', dtype=np.float32, mode='w+', shape=(N,))
+
+        #self.pr1 = np.memmap('/tmp/v0', dtype=np.float32, mode='w+', shape=(N,))
+        #self.pr2 = np.memmap('/tmp/v1', dtype=np.float32, mode='w+', shape=(N,))
+        #self.out_degree = np.memmap('/tmp/v2', dtype=np.float32, mode='w+', shape=(N,))
 
         self.pr1[:] = 1.0/n_pages
         self.pr2[:] = 0.0
         self.out_degree[:] = 0.0
 
+    @cython.boundscheck(False)
+    @cython.wraparound(False)
     def degree(self, edges):
         cdef np.int64_t a = 0
         cdef np.ndarray[Edge, ndim=1] block
-        cdef int i
+        cdef Py_ssize_t i
         cdef np.ndarray[np.float32_t, ndim=1] deg = self.out_degree
 
         for block in bcolz.iterblocks(edges):
-            for i in xrange(len(block)):
+            for i in range(block.shape[0]):
                 a += block[i].start
                 deg[a] += 1.0
 
@@ -36,18 +44,22 @@ class PageRank(object):
         self.pr1 *= self.damping/self.out_degree
         self.pr2[:] = (1.0 - self.damping)/self.n_pages        
 
+    @cython.boundscheck(False)
+    @cython.wraparound(False)
     def update(self, edges):
-        cdef int i
+        cdef Py_ssize_t i
         cdef np.int64_t a = 0
         cdef np.int64_t b = 0
+        cdef Edge edge
         cdef np.ndarray[Edge, ndim=1] block
         cdef np.ndarray[np.float32_t, ndim=1] pr1 = self.pr1
         cdef np.ndarray[np.float32_t, ndim=1] pr2 = self.pr2
 
         for block in bcolz.iterblocks(edges):
-            for i in xrange(len(block)):
-                a += block[i].start
-                b += block[i].end
+            for i in range(block.shape[0]):
+                edge = block[i]
+                a += edge.start
+                b += edge.end
                 pr2[b] += pr1[a]
 
     def end_updates(self):
