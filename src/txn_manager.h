46855fa7a770468a90227cc26de26caceed8e2aa
Pedro Lopez-Adeva <plopezadeva@gmail.com>
2015-04-23 10:00:35 +0200

    Some header comments.

--- src/txn_manager.h
+++ src/txn_manager.h
@@ -6,6 +6,11 @@
 
 #include "util.h"
 
+/** Inverse Semaphore. 
+ *
+ * An inverse semaphore blocks when the count is greater than zero (a regular
+ * semaphore blocks when the count is at zero).
+ */
 typedef struct {
      pthread_cond_t cond;
      pthread_mutex_t mtx_inc_dec;
@@ -13,59 +18,131 @@ typedef struct {
      int value;
 } InvSemaphore;
 
+/** Initialize.
+ *
+ *  @return A pthread error code
+ */
 int
 inv_semaphore_init(InvSemaphore *is);
 
+/** Increment counter. 
+ *
+ * This call will be blocked if the semaphore is blocked.
+ *  @return A pthread error code
+ */
 int
 inv_semaphore_inc(InvSemaphore *is);
 
+/** Decrement counter. 
+ *
+ *  @return A pthread error code
+ */
 int
 inv_semaphore_dec(InvSemaphore *is);
 
+/** Return the count */
 int
 inv_semaphore_count(InvSemaphore *is);
 
+/** Block the semaphore until count reaches zero.
+ *
+ * While the semaphore is blocked @ref inv_semaphore_inc is blocked
+ * but @ref inv_semaphore_dec is allowed.
+ *
+ * @return A pthread error code
+ */
 int
 inv_semaphore_block(InvSemaphore *is);
 
+/** Cancel the effect of @ref inv_semaphore_block 
+ *
+ * @return A pthread error code
+ */
 int
 inv_semaphore_release(InvSemaphore *is);
 
+/** Destroy semaphore (does NOT free memory) */
 int
 inv_semaphore_destroy(InvSemaphore *is);
 
 typedef enum {
-     txn_manager_error_ok = 0,
-     txn_manager_error_internal,
-     txn_manager_error_memory,
-     txn_manager_error_thread,
-     txn_manager_error_mdb
+     txn_manager_error_ok = 0,   /**< No error */
+     txn_manager_error_internal, /**< Unexpected error */
+     txn_manager_error_memory,   /**< Error allocating new memory */
+     txn_manager_error_thread,   /**< Error inside pthreads */
+     txn_manager_error_mdb       /**< Error inside LMDB */
 } TxnManagerError;
 
+/** Transaction Manager.
+ *
+ * LMDB has several restrictions in the operations it allows in multiple threads,
+ * but some of these restrictions must be imposed in the application code.
+ * In particular:
+ *
+ *     1. Some operations require that no transactions in the same process are 
+ *        active, for example mdb_env_set_mapsize
+ *     2. Some operations require that no write transactions are active. For 
+ *        example it is not documented, but it seems to happen that, mdb_env_info
+ *        crashes if write transactions are active.
+ *
+ * This structure tracks the number of read and write transactions active inside the 
+ * process and allows blocking until all of them are aborted or commited.
+ */
 typedef struct {
-     MDB_env *env;     
-     InvSemaphore txn_counter_read;
-     InvSemaphore txn_counter_write;
+     MDB_env *env; /**< LMDB environment where transactions happen */
+     InvSemaphore txn_counter_read;  /**< Counter of read transactions */
+     InvSemaphore txn_counter_write; /**< Counter of write transactions */
+
      Error error;
 } TxnManager;
 
+/** Allocate a new TxnManager */
 TxnManagerError
 txn_manager_new(TxnManager **tm, MDB_env *env);
 
+/** Begin a new transaction.
+ *
+ * Flags are the flags that you pass to LMDB's mdb_txn_begin. These flags
+ * will be checked for MDB_RDONLY to decide which transaction counter to 
+ * increment. This transaction will block if an environment resize is in progress.
+ */
 TxnManagerError
 txn_manager_begin(TxnManager *tm, int flags, MDB_txn **txn);
 
+/** Commit transaction.
+ *
+ * The corresponding counter will be decremented
+ */
 TxnManagerError
 txn_manager_commit(TxnManager *tm, MDB_txn *txn);
 
+/** Abort transaction.
+ *
+ * The corresponding counter will be decremented
+ */
 TxnManagerError
 txn_manager_abort(TxnManager *tm, MDB_txn *txn);
 
+/** Destroy and free manager */
 TxnManagerError
 txn_manager_delete(TxnManager *tm);
 
+/** Parameter associated to @ref txn_manager_expand.
+ * 
+ * The mmap is resized when the remaining free space is less than this amount.
+ */
 #define MDB_MINIMUM_FREE_PAGES 10000
 
+/** Check if the environment must be resized. If this is the case then resize 
+ *  it.
+ *
+ * This call will block for sure until there are no write transactions active.
+ * This call may block until there are no read transactions active, only if a resize
+ * is necessary.
+ *
+ * If a resize happens then creation of new read and write transactions will be 
+ * blocked until it finishes.
+ */
 TxnManagerError
 txn_manager_expand(TxnManager *tm);
 
--- src/util.h
+++ src/util.h
@@ -4,8 +4,15 @@
 #include <pthread.h>
 #include "lmdb.h"
 
-#define MAX_ERROR_LENGTH 10000
-
+/** Posible states of a stream.
+ *
+ *  ---> Error <-----+
+ *        ^          |
+ *        |          |
+ *  ---> Init ----> Next --+-> End
+ *             ^           |
+ *             +-----------+
+ */
 typedef enum {
      stream_state_init, /**< Stream ready */
      stream_state_next, /**< A new element has been obtained */
@@ -13,36 +20,79 @@ typedef enum {
      stream_state_error /**< Unexpected error */
 } StreamState;
 
+/// @addtogroup Error
+/// @{
+#define MAX_ERROR_LENGTH 10000
 typedef struct {
-     /** Make operations on errors atomic. Errors in threading ops
-         will be silently ignored */
+     /** Make operations on errors atomic. 
+      *
+      * If an error is produced dealing with this mutex it will be silently 
+      * ignored 
+      */
      pthread_mutex_t mtx;
 
+     /** Error code, depends on the application but 0 always signals no error */
      int code;
+     /** A descriptive message associated with the error code. If no error then 
+      *  it contains "NO ERROR" */
      char message[MAX_ERROR_LENGTH + 1];
 } Error;
 
+/** Initialize structure */
 void
 error_init(Error *error);
 
+/** Clean up. Will NOT free `error` */
 void
 error_destroy(Error *error);
 
+/** Set error.
+ *
+ * If an error is already present then do nothing. If you want to overwrite an
+ * already existing error then first call @ref error_clean
+ */
 void
 error_set(Error* error, int code, const char *msg);
 
+/** Clean error.
+ *
+ * Error code is set to 0 and the message to NO ERROR.
+ */
 void
 error_clean(Error *error);
 
+/** Add a description message to the existing message and leaves as is the error 
+ * code */
 void
 error_add(Error* error, const char *msg);
 
+/// @}
+
+/** Returns a newly allocated string made by concatenating two strings
+ * with a separator.
+ *
+ * @param s1 The first string
+ * @param s2 The second string
+ * @param separator The separator
+ *
+ * @return A malloced new string: s1 + separator + s2. NULL if error.
+ */
 char *
 concat(const char *s1, const char *s2, char separator);
 
+/** Concatenates `path` and `fname` with the filesystem separator.
+ *
+ * @return path + '/' fname
+ */
 char *
 build_path(const char *path, const char *fname);
 
+/** Make a new directory at `path` if not already present.
+ *
+ * Permissions are set to 0664
+ *
+ * @return An strerror with a description of the error or NULL if success.
+ */
 char *
 make_dir(const char *path);
 
