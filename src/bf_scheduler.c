7841b7dd95fdc642bafba40317e5e10e808f8682
Pedro Lopez-Adeva <plopezadeva@gmail.com>
2015-05-05 16:32:13 +0200

    MDB_NOSYNC is the only flag that gives decent performance in Windows.

--- src/bf_scheduler.c
+++ src/bf_scheduler.c
@@ -138,7 +138,7 @@ bf_scheduler_new(BFScheduler **sch, PageDB *db) {
      else if ((rc = mdb_env_open(
                     p->txn_manager->env,
                     p->path,
-                    MDB_NOTLS | MDB_WRITEMAP | MDB_MAPASYNC, 0664) != 0))
+                    MDB_NOTLS | MDB_NOSYNC, 0664) != 0))
           error = "opening environment";
 
      if (error != 0) {
