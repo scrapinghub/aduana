179096d3554f432c15cb5d6f29e81cf5491ee624
Pedro Lopez-Adeva <plopezadeva@gmail.com>
2015-06-19 16:00:16 +0200

    Added WebBackend settings in example.

--- example/example/frontera/settings.py
+++ example/example/frontera/settings.py
@@ -9,14 +9,10 @@ PAGE_DB_PATH = 'test-crawl'
 SCORER = 'HitsScorer'
 USE_SCORES = True
 
-# Only used if BACKEND = 'aduana.frontera.WebBackend'
-SERVER_NAME = 'localhost'
-SERVER_PORT = 8000
-SERVER_CERT = 'aduana-server.crt'
 #--------------------------------------------------------
 # Logging
 #--------------------------------------------------------
 LOGGING_EVENTS_ENABLED = False
 LOGGING_MANAGER_ENABLED = False
-LOGGING_BACKEND_ENABLED = False
+LOGGING_BACKEND_ENABLED = True
 LOGGING_DEBUGGING_ENABLED = False
---
+++ example/example/frontera/web_settings.py
@@ -0,0 +1,8 @@
+from settings import *
+
+BACKEND = 'aduana.frontera.WebBackend'
+
+SERVER_NAME = 'localhost'
+SERVER_PORT = 8000
+SERVER_CERT = 'aduana-server.crt'
+
