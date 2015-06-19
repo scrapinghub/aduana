80c5f4a26da56b96e4e0446fb84c23d2c830e958
Pedro Lopez-Adeva <plopezadeva@gmail.com>
2015-06-19 10:48:31 +0200

    Added example settings in case WebBackend is used.

--- example/example/frontera/settings.py
+++ example/example/frontera/settings.py
@@ -5,9 +5,14 @@ BACKEND = 'pdb_frontera.Backend'
 MAX_REQUESTS = 100000000
 MAX_NEXT_REQUESTS = 10
 DELAY_ON_EMPTY = 0.0
-PAGE_DB_PATH = 'test_crawl'
+PAGE_DB_PATH = 'test-crawl'
 SCORER = 'HitsScorer'
 USE_SCORES = True
+
+# Only used if BACKEND = 'pdb_frontera.WebBackend'
+SERVER_NAME = 'localhost'
+SERVER_PORT = 8000
+SERVER_CERT = 'aduana-server.crt'
 #--------------------------------------------------------
 # Logging
 #--------------------------------------------------------
